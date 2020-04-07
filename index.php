bf9a5ba96ace0a15adffbbc27ac7f63aea8985a1
Matilda Söderblom <soderblom.matilda@gmail.com>
2020-04-07 18:11:31 +0200

    Made a change on branch

--- index.php
+++ index.php
@@ -7,6 +7,7 @@ echo "<p>Branches is awesome!</p>";
 
 echo "<p>Test from Matilda!!</p>";
 echo "<p>Test from Matilda's branch</p>";
+echo "<p>Test from Matilda's branch 2</p>";
 
 echo "<p>Halloj säger Henningsonen</p>";
 
