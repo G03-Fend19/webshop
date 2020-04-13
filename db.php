42ebf049931c5373661ffefa4d0d90a3cf7b2f9c
Matilda Söderblom <soderblom.matilda@gmail.com>
2020-04-13 14:33:05 +0200

    gitignore test

--- db.php
+++ db.php
@@ -6,7 +6,7 @@ error_reporting(E_ALL);
 $db_server = "localhost";
 $db_database = "ws_db";
 $db_username = "root";
-$db_password = "root";
+$db_password = "";
 
 try {
     $db = new PDO("mysql:host=$db_server;dbname=$db_database;charset=utf8", $db_username, $db_password);
@@ -16,3 +16,4 @@ try {
 } catch (PDOException $e) {
     echo $e->getMessage();
 }
+//test
