cb1424349502dc449881f8e62b168943e2847a47
Evelina Björkman <evelina.v.bjorkman@gmail.com>
2020-04-14 16:10:57 +0200

    Added category table and edit functionality

--- admin/assets/category-table.php
+++ admin/assets/category-table.php
@@ -10,34 +10,61 @@ $stmt->execute();
 
 $categories = "";
 
-while ($row = $stmt->fetch(PDO::FETCH_ASSOC)):
+while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+
     $name = htmlspecialchars($row['name']);
+    $id = htmlspecialchars($row['id']);
 
-    $categories .= "<tr>
-				                      <th>
-				                        $name
-				                      </th>
-				                      <th>
-				                        <form action='#' method'POST'>
-				                          <button type='submit'>Edit</button>
-				                        </form>
-				                      </th>
-				                      <th>
-				                        <form action='#' method'POST'>
-				                          <button type='submit'>Delete</button>
-				                        </form>
-				                      </th>
-				                    </tr>";
+    $edit = "<input type='text' value='$name'>";
 
-endwhile;
+    $toggle = $name;
+
+    $categories .= "<form action='edit-category.php' method='POST'>
+    <tr>
+                  <td>
+                    <input class='hidden' type='text' name='name' id='input-$id' value='$name'>
+                    <p id='category-$id'>$name<p>
+                  </td>
+                  <td>
+                    <button type='button' id='editBtn-$id' onclick='editCategory($id);'>Edit</button>
+
+                      <button class='hidden' type='submit' id='saveBtn-$id'>Save</button>
+                      <input type='hidden' name='id' value='$id'>
+
+                  </td>
+                  <td>
+                    <button>Delete</button>
+                  </td>
+                </tr>
+                </form>";
+}
 
 ?>
-
 
 <h1>Categories</h1>
 
 <table>
-  <tbody>
-    <?php echo $categories ?>
-  </tbody>
+  <?php echo $categories; ?>
 </table>
+
+<script>
+function editCategory(id) {
+  const categoryP = document.querySelector('#category-' + id);
+  categoryP.classList.toggle('hidden');
+
+  const input = document.querySelector('#input-' + id);
+  input.classList.toggle('hidden');
+
+  const editBtn = document.querySelector('#editBtn-' + id);
+  editBtn.classList.toggle('hidden');
+
+  const saveBtn = document.querySelector('#saveBtn-' + id);
+  saveBtn.classList.toggle('hidden');
+}
+</script>
+
+<style>
+  .hidden {
+    display: none;
+  }
+</style>
---
+++ admin/assets/edit-category.php
@@ -0,0 +1,22 @@
+<?php
+
+require_once '../../db.php';
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+
+    print_r($_POST);
+
+    $id = htmlspecialchars($_POST['id']);
+    $newName = htmlspecialchars($_POST['name']);
+
+    $sql = "UPDATE ws_categories SET name = :name
+    WHERE id = :id";
+
+    $stmt = $db->prepare($sql);
+    $stmt->bindParam(':name', $newName);
+    $stmt->bindParam(':id', $id);
+
+    $stmt->execute();
+}
+
+header('Location: category-table.php');
