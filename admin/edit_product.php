a7988e149669e489257360cff7fafe838978152c
Matilda Söderblom <soderblom.matilda@gmail.com>
2020-04-17 17:53:44 +0200

    Fixed error when adding img in update

--- admin/edit_product.php
+++ admin/edit_product.php
@@ -116,6 +116,13 @@ echo "</pre>"; */
 </head>
 
 <body>
+  <form id="dragme" class="upload-form hidden" method='post' action='' enctype='multipart/form-data' draggable="true">
+    <div class="upload-form__border"> <button class="cancel-upload" type="button">X</button> </div>
+    <input type="file" name="file[]" id="file" multiple>
+    <input type="hidden" name="p_id" value="<?=$productId?>">
+    <input class="upload-btn" type='submit' name='submit' value='Upload'>
+
+  </form>
   <form class="form" id="addProductForm" name="addProductForm" action="./assets/process_product_edit.php"
     onsubmit="return validateProductForm()" method="POST">
     <div class="form__group">
