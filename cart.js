cfcadc4684671b047335b70410fe9c389c385a9c
Andreas <Hellberg.andreas@gmail.com>
2020-04-21 19:49:52 +0200

    JS cleanup for cart

--- cart.js
+++ cart.js
@@ -16,11 +16,13 @@
       createProduct(productData)
     })
   )
-  // we check the cart object if product we want to add already exists, if so pressing  add-product only increases
+
+  // we check the cart object if the product we want to add already exists, if so pressing  add-product only increases
   // quantity.
+  // if item is new to cart, we create a new cart variable, spread everything else back in, with the new product
   const createProduct = (productData) => {
     if (cart[productData.id]) {
-      cart[productData.id].quantity++
+      checkStock(productData.id)
     } else {
       cart = {
         ...cart,
@@ -38,11 +40,19 @@
     renderCart()
   }
 
+  // check stock takes current Product
+  // as long as quantity is lower than stock,  user is allowed to put more of that product in the cart.
+
+  const checkStock = (product) => {
+    const q = cart[product].quantity
+    const s = cart[product].stock
+    q < s ? cart[product].quantity++ : alert("out of stock")
+  }
+
   const calcTotal = () => {
-    let total = 0
-    Object.keys(cart).forEach((product) => {
-      total += cart[product].price * cart[product].quantity
-    })
+    total = Object.keys(cart).reduce((acc, cur) => {
+      return acc + cart[cur].price * cart[cur].quantity
+    }, 0)
     localStorage.setItem("total", JSON.stringify(total))
     return `<div>${total}</div>`
   }
@@ -58,14 +68,14 @@
     const fulCart = document.querySelector(".fulCart")
     fulCart.innerHTML = ""
 
-    fulCart.innerHTML += Object.keys(cart).map((el) => {
+    fulCart.innerHTML += Object.keys(cart).map((product) => {
       return `
-      <div data-id=${cart[el].id}>
+      <div data-id=${cart[product].id}>
       <button id="qty-" class="changeQty">-</button>
-      ${cart[el].quantity} * ${cart[el].name}
+      ${cart[product].quantity} * ${cart[product].name}
       <button id="qty+" class="changeQty">+</button>
       <button  class="delete-product">delete</button>
-      <div>${cart[el].quantity * cart[el].price}</div>
+      <div>${cart[product].quantity * cart[product].price}</div>
       </div>
       `
     })
@@ -76,7 +86,6 @@
   // a clicklistener on entire document. fires when user presses
   // anything with class changeQty
   // if the target id = + or -, we add or subtract 1 to corresponding products quantity in cart
-  // we also check against current stock-value. We cannot add more products than stock allow.
 
   const changeQty = () => {
     document.addEventListener("click", (e) => {
@@ -84,9 +93,7 @@
 
       if (e.target.className == "changeQty") {
         if (e.target.id == "qty+") {
-          cart[productId].quantity <= cart[productId].stock
-            ? cart[productId].quantity++
-            : alert("no more products in stock")
+          checkStock(productId)
         } else if (e.target.id == "qty-") {
           cart[productId].quantity == 1 ? null : cart[productId].quantity--
         }
