0b30ebf8b0faad3b4afecda48470d1bdb90bfa66
Matilda Söderblom <soderblom.matilda@gmail.com>
2020-04-22 14:18:21 +0200

    started creating product table

--- checkout_page.php
+++ checkout_page.php
@@ -1,3 +1,49 @@
 <?php
+require_once 'assets/header.php';
+?>
+<main class="checkout">
+  <section id="pTable-section" class="product-section">
+
+
+  </section>
+
+</main>
+
+
+
+
+
+<script>
+let order = JSON.parse(localStorage.cart);
+const productSection = document.querySelector('#pTable-section');
+console.log(productSection);
+
+if (order) {
+  let productTable = `<table>
+                        <thead>
+                          <tr>
+                            <th></th>
+                            <th></th>
+                            <th>Product name</th>
+                            <th>Quantity</th>
+                            <th>Price</th>
+                          </tr>
+                        </thead>
+                        `;
+
+  /*   <tbody>
+
+</tbody>
+</table> */
+} else {
+  /* <h4>No products in this category</h4> */
+}
+console.log(order);
+</script>
+
+
+
+<?php
+require_once 'assets/foot.php';
 
 ?>
