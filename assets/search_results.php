87264e7c2772ce559c367c9e062d5ddf4fede677
Evelina Björkman <evelina.v.bjorkman@gmail.com>
2020-04-29 15:46:54 +0200

    Added right functionality for NewIn i search page

--- assets/search_results.php
+++ assets/search_results.php
@@ -1,6 +1,11 @@
 <?php
 $search = "";
 
+$currentDateTime = date('Y-m-d H:i:s');
+$currentDateTimeDT = new DateTime($currentDateTime);
+$newInLimitDT = $currentDateTimeDT->sub(new DateInterval('P14D'));
+$newInLimitDate = $newInLimitDT->format('Y-m-d H:i:s');
+
 if (isset($_GET['search']) && $_GET['search'] !== "") {
     require_once "./db.php";
     $search = htmlspecialchars($_GET['search']);
@@ -10,6 +15,7 @@ if (isset($_GET['search']) && $_GET['search'] !== "") {
             ws_products.price         AS ProductPrice,
             ws_products.id            AS ProductId,
             ws_products.stock_qty     AS ProductQty,
+            ws_products.added_date    AS AddedDate,
             ws_images.img             AS ImageName,
             ws_products_images.img_id AS ProductImageImageId
           FROM
@@ -51,10 +57,9 @@ if (isset($_GET['search']) && $_GET['search'] !== "") {
     //   ]
     // ]
     $grouped = [];
-    $index = 0;
 
     foreach ($results as $row) {
-      $index = $index + 1;
+        $index = $index + 1;
         // The product id for this row
         $currentProductId = $row["ProductId"];
 
@@ -71,7 +76,7 @@ if (isset($_GET['search']) && $_GET['search'] !== "") {
                 "ProductName" => $row["ProductName"],
                 "ProductPrice" => $row["ProductPrice"],
                 "ProductQty" => $row["ProductQty"],
-                "IndexInArray" => $index,
+                "AddedDate" => $row['AddedDate'],
             ];
 
             // If there is an image for this row, add it
@@ -81,16 +86,17 @@ if (isset($_GET['search']) && $_GET['search'] !== "") {
         }
     }
 
-    $limitForNew = count($grouped) - 5;
-$newProductMsg = "";
+    $newProductMsg = "";
 
     foreach ($grouped as $productId => $product):
-      if ($product['IndexInArray'] > $limitForNew) {
+      if ($product['AddedDate'] >= $newInLimitDate) {
         $newProductMsg = "<div class='new-in'>
 		                        <span class='new-in__msg'>
 		                        New In
 		                        </span>
 		                      </div>";
+    }
+
         $productName = htmlspecialchars($product['ProductName']);
         if (strlen($productName) > 20) {
             $productName = substr($productName, 0, 20) . "...";
@@ -105,33 +111,33 @@ $newProductMsg = "";
         }
 
         $productCards .= "<article class='product-card'>
-			                        <a href='product.php?product_id=$productId#main' class='product-card__image-link'>
-                                <div class='image-wrapper'>
-                                $newProductMsg";
+						                        <a href='product.php?product_id=$productId#main' class='product-card__image-link'>
+			                                <div class='image-wrapper'>
+			                                $newProductMsg";
         $productQty < 1 ? $productCards .= "<div class='out-of-stock'>
-			                                                            <span class='out-of-stock__msg'>
-			                                                            Product currently out of stock
-			                                                            </span>
-			                                                          </div>" : null;
+						                                                            <span class='out-of-stock__msg'>
+						                                                            Product currently out of stock
+						                                                            </span>
+						                                                          </div>" : null;
         $productCards .= "<img class='product-thumb' src=./media/product_images/$productImg alt=''>
-			                          </div>
-			                        </a>
-			                        <div class='product-card__content'>
-			                          <a href='product.php?product_id=$productId#main' class='product-card__product-link'>
-			                            $productName
-			                          </a>
-			                          <p>$productPrice SEK</p>
-			                          <button
-			                          	 data-id=$productId
-					                          data-name='$productName'
-					                          data-price=$productPrice
-					                          data-img='$productImg'
-					                          data-stock=$productQty
-			                          class='add-to-cart-btn'>";
+						                          </div>
+						                        </a>
+						                        <div class='product-card__content'>
+						                          <a href='product.php?product_id=$productId#main' class='product-card__product-link'>
+						                            $productName
+						                          </a>
+						                          <p>$productPrice SEK</p>
+						                          <button
+						                          	 data-id=$productId
+								                          data-name='$productName'
+								                          data-price=$productPrice
+								                          data-img='$productImg'
+								                          data-stock=$productQty
+						                          class='add-to-cart-btn'>";
         $productQty < 1 ? $productCards .= "<i class='far fa-times-circle'></i>" : $productCards .= "<i class='fas fa-cart-plus'></i>";
         $productCards .= "</button>
-			                          </div>
-			                      </article>";
+						                          </div>
+						                      </article>";
     endforeach;
     $productsContainer .= $productCards;
     $productsContainer .= "</div>";
