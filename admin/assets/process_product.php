bbdb49066d2812fa623e086bec36d64c2cc13df4
Matilda Söderblom <soderblom.matilda@gmail.com>
2020-04-30 14:26:08 +0200

    Added sql for adding feature-img in db

--- admin/assets/process_product.php
+++ admin/assets/process_product.php
@@ -8,7 +8,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $category = htmlspecialchars($_POST['category']);
     $price = htmlspecialchars($_POST['price']);
     $qty = htmlspecialchars($_POST['qty']);
-    $feature = htmlspecialchars($_POST['feature']);
+    $featureImg = htmlspecialchars($_POST['feature']);
 
     //print_r($_POST);
     $images = [];
@@ -17,71 +17,77 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
         if (isset($_POST["image$i"])) {
             $images += ["image$i" => $_POST["image$i"]];
         }
-      
-    } 
-     print_r($images);
-     echo($feature);
 
-//     $duplicateCheck = "SELECT name FROM ws_products
-// WHERE name = :title
-// AND active = 1";
+    }
+    /* print_r($images);
+    echo($featureImg); */
 
-//     $stmt = $db->prepare($duplicateCheck);
-//     $stmt->bindParam(':title', $title);
+    $duplicateCheck = "SELECT name FROM ws_products
+WHERE name = :title
+AND active = 1";
 
-//     $stmt->execute();
-//     if (empty($title) || empty($description) || $price == "" || $price == null || $qty == "" || $qty == null) {
-//         header("Location: ../create_product.php?formerror=empty&title=$title&descrip=$description&price=$price&qty=$qty");
-//         exit();
-//     } elseif ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
-//         header("Location: ../create_product.php?formerror=duplicate&title=$title&descrip=$description&price=$price&qty=$qty");
-//         exit();
-//     } elseif ($category == "category" || empty($category)) {
-//         header("Location: ../create_product.php?formerror=nocategory&title=$title&descrip=$description&price=$price&qty=$qty");
-//         exit();
-//     } elseif ($price < 0 || $qty < 0) {
-//         header("Location: ../create_product.php?formerror=negative&title=$title&descrip=$description&price=$price&qty=$qty");
-//         exit();
-//     }
-//     ;
+    $stmt = $db->prepare($duplicateCheck);
+    $stmt->bindParam(':title', $title);
 
-// //Inserting the new product into db
-//     $sql1 = "INSERT INTO ws_products (name, description, price, stock_qty)
-// VALUES (:name, :description, :price, :qty)";
+    $stmt->execute();
+    if (empty($title) || empty($description) || $price == "" || $price == null || $qty == "" || $qty == null) {
+        header("Location: ../create_product.php?formerror=empty&title=$title&descrip=$description&price=$price&qty=$qty");
+        exit();
+    } elseif ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+        header("Location: ../create_product.php?formerror=duplicate&title=$title&descrip=$description&price=$price&qty=$qty");
+        exit();
+    } elseif ($category == "category" || empty($category)) {
+        header("Location: ../create_product.php?formerror=nocategory&title=$title&descrip=$description&price=$price&qty=$qty");
+        exit();
+    } elseif ($price < 0 || $qty < 0) {
+        header("Location: ../create_product.php?formerror=negative&title=$title&descrip=$description&price=$price&qty=$qty");
+        exit();
+    }
+    ;
 
-//     $stmt1 = $db->prepare($sql1);
+//Inserting the new product into db
+    $sql1 = "INSERT INTO ws_products (name, description, price, stock_qty)
+VALUES (:name, :description, :price, :qty)";
 
-//     $stmt1->bindParam(':name', $title);
-//     $stmt1->bindParam(':description', $description);
-//     $stmt1->bindParam(':price', $price);
-//     $stmt1->bindParam(':qty', $qty);
+    $stmt1 = $db->prepare($sql1);
 
-//     $stmt1->execute();
+    $stmt1->bindParam(':name', $title);
+    $stmt1->bindParam(':description', $description);
+    $stmt1->bindParam(':price', $price);
+    $stmt1->bindParam(':qty', $qty);
 
-// //Inserting the category relationship
-//     $sql2 = "INSERT INTO ws_products_categories (product_id, category_id)
-// VALUES (LAST_INSERT_ID(), :category);
-// SET @p_id = LAST_INSERT_ID()";
+    $stmt1->execute();
 
-//     $stmt2 = $db->prepare($sql2);
-//     $stmt2->bindParam(':category', $category);
-//     $stmt2->execute();
+//Inserting the category relationship
+    $sql2 = "INSERT INTO ws_products_categories (product_id, category_id)
+VALUES (LAST_INSERT_ID(), :category);
+SET @p_id = LAST_INSERT_ID()";
 
-// //Inserting the images and product img relationship
+    $stmt2 = $db->prepare($sql2);
+    $stmt2->bindParam(':category', $category);
+    $stmt2->execute();
 
-//     if (count($images) != 0) {
-//         foreach ($images as $index => $img) {
-//             $sql_img = "INSERT INTO ws_images (img) VALUES (:img)";
-//             $stmt = $db->prepare($sql_img);
-//             $stmt->bindParam(":img", $img);
-//             $stmt->execute();
+//Inserting the images and product img relationship
 
-//             $sql_p_img = "INSERT INTO ws_products_images (product_id, img_id)
-//                     VALUES ( @p_id, LAST_INSERT_ID())";
-//             $stmt_rel = $db->prepare($sql_p_img);
-//             $stmt_rel->execute();
-//         }
-//     }
+    if (count($images) != 0) {
+        foreach ($images as $index => $img) {
+            $sql_img = "INSERT INTO ws_images (img) VALUES (:img)";
+            $stmt = $db->prepare($sql_img);
+            $stmt->bindParam(":img", $img);
+            $stmt->execute();
+
+            if ($img == $featureImg) {
+                $sql_p_img = "INSERT INTO ws_products_images (product_id, img_id, feature)
+                    VALUES ( @p_id, LAST_INSERT_ID(), 1)";
+            } else {
+                $sql_p_img = "INSERT INTO ws_products_images (product_id, img_id)
+                    VALUES ( @p_id, LAST_INSERT_ID())";
+            }
+
+            $stmt_rel = $db->prepare($sql_p_img);
+            $stmt_rel->execute();
+        }
+    }
 
 }
 
