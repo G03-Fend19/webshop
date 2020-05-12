539b148b3088683293d8199dcd0068646d6d28e8
Evelina Björkman <evelina.v.bjorkman@gmail.com>
2020-05-12 13:42:32 +0200

    Fixed overview

--- admin/order_page_functions.js
+++ admin/order_page_functions.js
@@ -13,7 +13,6 @@ if (position) {
 //   console.log(row)
 //   localStorage.removeItem('row');
 // }
-
 function filterOrders(orders) {
   let orderType;
   Object.keys(orders).forEach(function (order) {
@@ -22,18 +21,14 @@ function filterOrders(orders) {
   //Active orders filter select/input element
   const activeStatusFilter = document.querySelector("#activeStatusFilter");
   const activeTextFilter = document.querySelector("#activeTextFilter");
-
   //Completed orders filter input element
   const completedTextFilter = document.querySelector("#completedTextFilter");
-
   //Active/Completed orders tables
   const activeOrdersTable = document.querySelector("#activeOrdersTable");
   const completedOrdersTable = document.querySelector("#completedOrdersTable");
-
   let ordersToDraw = orders;
   let filterText;
   let filterStatus;
-
   //Checks if list of orders is active orders
   if (orderType == "active") {
     activeOrdersTable.innerHTML = "";
@@ -55,7 +50,6 @@ function filterOrders(orders) {
         .includes(filterText.toLowerCase());
     });
   }
-
   //If filterStatus is not 'all'
   if (filterStatus && filterStatus !== "all") {
     //filter ordersToDraw if chosen status matches order['OrderStatusId']
@@ -63,35 +57,27 @@ function filterOrders(orders) {
       return order["OrderStatusId"].includes(filterStatus);
     });
   }
-
   Object.values(ordersToDraw).forEach(function (order, i) {
     const tr = document.createElement("tr");
-
     const tdId = document.createElement("td");
     tdId.innerHTML = "#" + order["OrderNumber"];
-
     const tdCustomer = document.createElement("td");
     let fullName = order["CustomerFirstName"] + " " + order["CustomerLastName"];
     const fullNameShort = fullName.substring(0, 20) + "...";
     fullName.length > 20 ? (fullName = fullNameShort) : (fullName = fullName);
     tdCustomer.innerHTML = fullName;
-
     const tdCity = document.createElement("td");
     tdCity.innerHTML = order["DeliveryCity"];
-
     const tdDate = document.createElement("td");
     tdDate.innerHTML = order["OrderDate"];
-
     const tdSum = document.createElement("td");
     tdSum.innerHTML = order["OrderCost"] + " SEK";
-
     const tdStatus = document.createElement("td");
     if (order.OrderType == "active") {
       const statusForm = document.createElement("form");
       statusForm.setAttribute("id", "shouldUpdate" + order["OrderNumber"]);
       statusForm.setAttribute("action", "./assets/update_order_status.php");
       statusForm.setAttribute("method", "POST");
-
       const statusSelect = document.createElement("select");
       statusSelect.setAttribute("name", "statusSelect" + order["OrderNumber"]);
       statusSelect.setAttribute("id", "statusSelect" + order["OrderNumber"]);
@@ -100,7 +86,6 @@ function filterOrders(orders) {
         "updateStatus(" + order["OrderNumber"] + ")"
       );
       statusSelect.classList.add("select-status");
-
       const statusOPending = document.createElement("option");
       statusOPending.innerHTML = "Pending";
       statusOPending.setAttribute("value", "1");
@@ -129,7 +114,6 @@ function filterOrders(orders) {
       let returnUrl = window.location.href;
       // console.log(returnUrl);
       hiddenReturnURLInput.setAttribute("value", returnUrl);
-
       statusSelect.appendChild(statusOPending);
       statusSelect.appendChild(statusOInProgress);
       statusSelect.appendChild(statusOCompleted);
@@ -140,27 +124,110 @@ function filterOrders(orders) {
     } else {
       tdStatus.innerHTML = order["OrderStatus"];
     }
-
     const tdOpen = document.createElement("td");
-    const formLink = document.createElement("form");
-    formLink.setAttribute("action", "");
-    formLink.setAttribute("method", "POST");
-
+    // const formLink = document.createElement("form");
+    // formLink.setAttribute("action", "");
+    // formLink.setAttribute("method", "POST");
     const openOrderBtn = document.createElement("button");
-    openOrderBtn.setAttribute("id", "openModal");
+    // openOrderBtn.setAttribute("id", "openModal" );
     openOrderBtn.classList.add("open-modal");
-
     openOrderBtn.innerHTML = "<i class='far fa-eye'></i>";
+    const moodal = document.createElement("div");
+    moodal.setAttribute("id", "completedOrdersModal");
+    moodal.setAttribute("class", "order_overview");
 
+    let sale = "";
+
+    if (
+      Date.parse(order["Products"][0].ProductDate) < Date.parse("-1 year") &&
+      order["Products"][0].ProductStock < 10
+    ) {
+      sale = "yes";
+    } else {
+      sale = "no";
+    }
+
+    let productTable = "";
+
+    for (let i = 0; i < order["Products"].length; i++) {
+      productTable +=
+        `<tr><td> ` +
+        order["Products"][i].ProductName +
+        `</td><td> ` +
+        order["Products"][i].ProductDesc +
+        `</td><td> ` +
+        order["Products"][i].ProductPrice +
+        `</td><td> ` +
+        order["Products"][i].ProductQty +
+        `</td><td> ` +
+        sale +
+        `</td></tr>`;
+    }
+
+    moodal.innerHTML =
+      `
+      <div class='order_overview__content'>
+        <div class='order_overview__content__header'>
+          <span class='close'>&times;</span>
+          <h2>Order overview</h2> 
+        </div>
+        <div class='order_overview__content__body'>
+        <p>#` +
+      order["OrderNumber"] +
+      `</p>
+        <p>` +
+      order["CustomerFirstName"] +
+      " " +
+      order["CustomerLastName"] +
+      `</p>
+        <p>` +
+      order["DeliveryStreet"] +
+      `</p> 
+        <p>` +
+      order["DeliveryPostal"] +
+      `</p>
+        <p>` +
+      order["DeliveryCity"] +
+      `</p>
+        <p>` +
+      order["OrderDate"] +
+      `</p>
+        <table>
+          <thead>
+            <tr>
+              <td>Product</td>
+              <td>Description</td>
+              <td>Price</td>
+              <td>Quantity</td> 
+              <td>On Sale</td>
+            </tr>
+          </thead>
+          <tbody>
+          ` +
+      productTable +
+      `
+          </tbody>
+        </table>
+        <p>Total price:</p>
+        <p>` +
+      order["OrderCost"] +
+      " SEK" +
+      `</p>
+        </div>
+        <div class='order_overview__content__footer'>
+        <button class='cancel-btn'>Close</button>  
+        </div>
+      </div>`;
     const hiddenOpenOrderInput = document.createElement("input");
     hiddenOpenOrderInput.setAttribute("type", "hidden");
     hiddenOpenOrderInput.setAttribute("name", "o_id");
     hiddenOpenOrderInput.setAttribute("value", order["OrderNumber"]);
-
-    formLink.appendChild(openOrderBtn);
-    formLink.appendChild(hiddenOpenOrderInput);
-    tdOpen.appendChild(formLink);
-
+    // formLink.appendChild(openOrderBtn);
+    // formLink.appendChild(moodal);
+    // formLink.appendChild(hiddenOpenOrderInput);
+    // tdOpen.appendChild(formLink);
+    tdOpen.appendChild(openOrderBtn);
+    tdOpen.appendChild(moodal);
     tr.appendChild(tdId);
     tr.appendChild(tdCustomer);
     tr.appendChild(tdCity);
@@ -172,8 +239,30 @@ function filterOrders(orders) {
       : completedOrdersTable.appendChild(tr);
     tr.appendChild(tdOpen);
   });
+  if (typeof openOrderBtn != "undefined") {
+  }
+  document.querySelectorAll(".open-modal").forEach((item) => {
+    item.addEventListener("click", (event) => {
+      let currentModal = event.currentTarget.nextElementSibling;
+      currentModal.style.display = "block";
+      window.onclick = function (event) {
+        if (event.target == currentModal) {
+          currentModal.style.display = "none";
+        }
+      };
+      document.addEventListener("click", (e) => {
+        if (e.target.className == "close") {
+          currentModal.style.display = "none";
+        }
+      });
+      document.addEventListener("click", (e) => {
+        if (e.target.className == "cancel-btn") {
+          currentModal.style.display = "none";
+        }
+      });
+    });
+  });
 }
-
 function updateStatus(orderToUpdate) {
   const statusSelect = document.getElementById("statusSelect" + orderToUpdate);
   const newStatusId = statusSelect.options[statusSelect.selectedIndex].value;
@@ -184,7 +273,6 @@ function updateStatus(orderToUpdate) {
   const span = document.getElementsByClassName("close")[0];
   const cancelBtn = document.getElementById("cancel");
   const changeStatusBtn = document.getElementById("changeStatus");
-
   let shouldUpdate;
   if (newStatusId == "3") {
     changeStatusModal.style.display = "block";
@@ -205,7 +293,6 @@ function updateStatus(orderToUpdate) {
       shouldUpdate == false;
       filterOrders(activeOrdersFromPHP);
     });
-
     changeStatusBtn.addEventListener("click", (e) => {
       let pagePosition = window.pageYOffset;
       localStorage.setItem("page_position", pagePosition);
